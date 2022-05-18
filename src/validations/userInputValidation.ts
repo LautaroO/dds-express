538140a2fc39895292c883d3c2aa082966ca8c90
Lautaro <olivera4lautaro@gmail.com>
2022-05-18 11:33:56 -0300

    validacion dominio de email para crear el usuario

--- src/validations/userInputValidation.ts
+++ src/validations/userInputValidation.ts
@@ -1,6 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import { body, validationResult } from "express-validator";
 import { ErrorResponse } from "../dto/errorResponse";
+import { resolveMx } from "dns"
+
+
+
+const resolveMxRecord = (mx: string): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        resolveMx(mx, (error, mxs) => {
+            if (error || mxs.length === 0)
+                reject(false);
+            else
+                resolve(true);
+        });
+    });
+}
+const validateEmailDomain = async (email: string): Promise<boolean> => {
+    const splitEmail = email.split('@');
+    const mx = splitEmail[1];
+
+    return await resolveMxRecord(mx);
+}
 
 export const checkValidationErrors = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
@@ -14,4 +34,7 @@ export const checkValidationErrors = (req: Request, res: Response, next: NextFun
 export const userInputValidation = body('email')
     .exists().withMessage("El parametro email es requerido")
     .bail()
-    .isEmail().withMessage("El email no es valido");
+    .isEmail().withMessage("El email no es valido")
+    .bail()
+    .custom(validateEmailDomain).withMessage("El email no tiene un dominio valido");
+
